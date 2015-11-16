e160b1b17dafb5e56b58757b26df02648900601b
je-pbx <johannes.erber@photobox.com>
2015-11-16 15:55:31 +0100

    Migrate to react-hot-boilerplate webpack config
    Due to not working of hot reloading.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
-var getConfig = require('hjs-webpack')
+var path = require('path')
+var webpack = require('webpack')
 
-module.exports = getConfig({
-  in: 'src/index.js',
-  out: 'public',
-})
-
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+}
